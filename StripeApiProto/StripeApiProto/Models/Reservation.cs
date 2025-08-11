a497a1a393cb620f504a96106eabd954f87122a1
glorylaflare <juninho_toh@hotmail.com>
2025-08-11 08:02:39 -0300

    Reapply "feat: add projeto de teste do stripe para implementação dentro do projeto"
    This reverts commit 9d3cab28e2ca9e075c94daacdf51937d52190436.

---
+++ StripeApiProto/StripeApiProto/Models/Reservation.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StripeApiProto.Models
+{
+    public class Reservation
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public string? CoverImage { get; set; }
+        public bool IsConfirmed { get; set; }
+        public string UserEmail { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
