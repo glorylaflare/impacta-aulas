1805b87d78035ccfbf8a59fd633705333b6e913a
glorylaflare <juninho_toh@hotmail.com>
2025-08-11 07:45:10 -0300

    feat: add csharp project feito com scaffold

---
+++ AmoCSHARP/AmoCSHARP/Models/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace AmoCSHARP.Models;
+
+public class Produto
+{
+    public int Id { get; set; }
+    public string NomeProduto { get; set; }
+    public string NomeDescricao { get; set; }
+    public decimal Preco { get; set; } 
+    public int Estoque { get; set; }
+    public string Categoria { get; set; }
+}
